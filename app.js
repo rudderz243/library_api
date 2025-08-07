7d713e813265a6bfb410fc83cf0160f4a0029d89
Glynn Rudman <79942619+rudderz243@users.noreply.github.com>
2025-08-07 12:05:16 +0200

    25-08-07, we completed our routes, and added in some middleware to app.js

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 // calling in express to be used throughout the application
 const express = require('express');
 
+// call in our router
+const testRoutes = require('./routes/testRoutes.js');
+
 // setting up express using the default parameters
 const app = express();
 
+// calling in express.json middleware, so that our app can handle json
+app.use(express.json());
+
 // log every request
+// the logger will look at the request, generate a response, then handle the next incoming request
 app.use((req, res, next) => {
     // print out to the console (terminal) what type of method was used and to what endpoint that request was made
     console.log(`${req.method} ${req.url}`)
     // prepare to handle the next incoming request
     next();
-})
+});
+
+// first we version our api (v1) so that breaking changes can live on a new version
+// then we specify an area that we want the routes to live in (in this case /test)
+// finally, we point the app to where our routes live.
+app.use('/v1/test', testRoutes);
 
 // tell the API to start listening on a port we provide (which will eventually move to a .env file)
 app.listen(3000, () => {
     console.log(`The API is now listening on port 3000.`)
-})
+});
--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -1,3 +1,13 @@
 const express = require('express')
+// calling in our testController so that we can use its methods
+const testController = require('../controllers/testController.js')
 
+// set up the singleton for the router
 const router = express.Router();
+
+// here we first specify the type (.get for get request), we then specify what URL it lives at
+// and finally, we tell it which method in the controller will handle the logic
+router.get('/healthCheck', testController.healthCheck)
+
+// exporting our router (map), so that we can call it in the main program (app.js)
+module.exports = router
