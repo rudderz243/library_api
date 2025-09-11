a9950b9d6dce4be404bd63837a191a8d437d9daf
Glynn Rudman <79942619+rudderz243@users.noreply.github.com>
2025-09-11 09:19:39 +0200

    25-09-11 - installed axios, set up our api client singleton, and methods

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,28 @@
+// importing required react components
+import { useEffect, useState } from "react";
+// as well as our API methods we created
+import {
+  getAllBooks,
+  getBookById,
+  createBook,
+  updateBook,
+  deleteBook,
+} from '../services/apiService.js';
+
 // every page needs to return a default function, so that it can be called elsewhere
 export default function Dashboard() {
-    return(
-        <div>
-            <h1>Dashboard Page</h1>
-        </div>
-    )    
-};
+  const [books, setBooks] = useState([]);
+  const [selectedBookId, setSelectedBookId] = useState('');
+  const [formData, setFormData] = useState({
+    title: '',
+    author: '',
+    isbn: '',
+    edition: 0
+  });
+
+  return (
+    <div>
+      <h1>Dashboard Page</h1>
+    </div>
+  );
+}
